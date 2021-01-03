b0a23eeef4fe0face0d7e263b048b943a3b652c1
SebastianBabinski1 <sebastian.babinski1@gmail.com>
2021-01-03 23:22:55 +0100

    #14 Added visual image component

--- src/app/App.js
+++ src/app/App.js
@@ -1,2 +1,7 @@
-const App = ({ options }) => {};
+import VisualImage from "./components/VisualImage";
+
+const App = () => {
+  VisualImage("../../static/assets/img/modes/people/4.jpg");
+};
+
 export default App;
---
+++ src/app/components/VisualImage.js
@@ -0,0 +1,6 @@
+function VisualImage(imageSource) {
+  const img = document.querySelector(".visual__image")
+  img.setAttribute("src", imageSource);
+}
+
+export default VisualImage;
