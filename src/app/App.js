f4d8ad3bd1231cc4620e48605a1b80d8257e8e23
paula0403 <paulinalipinskagdy@gmail.com>
2021-01-04 00:16:02 +0100

    Logic has been added to the component MenuOptions, changes in style files.

--- src/app/App.js
+++ src/app/App.js
@@ -1,2 +1,8 @@
-const App = ({ options }) => {};
+
+import mainMenu from './Components/MenuOptions';
+
+const App = ({ options }) => {
+  const quiz = document.querySelector('#swquiz-app');
+  quiz.append(mainMenu);
+};
 export default App;
--- src/app/Components/MenuOptions.js
+++ src/app/Components/MenuOptions.js
@@ -1,1 +1,35 @@
-document.querySelectorAll("li");
+export default function MenuOptions() {
+  const mainMenu = document.createElement("div");
+  mainMenu.classList.add("container");
+  document.body.append(mainMenu);
+  const btnPeople = document.createElement("button");
+  btnPeople.setAttribute("data-option", "people");
+  btnPeople.classList.add("option");
+  btnPeople.textContent = "People";
+  const btnVehicles = document.createElement("button");
+  btnVehicles.setAttribute("data-option", "vehicles");
+  btnVehicles.classList.add("option");
+  btnVehicles.textContent = "Vehicles"
+  const btnStarships = document.createElement("button");
+  btnStarships.setAttribute("data-option", "starships");
+  btnStarships.classList.add("option");
+  btnStarships.textContent = "Starships"
+  divMenu.append(btnPeople, btnVehicles, btnStarships);
+
+  const menuOption = {
+    selectOption: ""
+  }
+  const options = [...document.querySelectorAll(".option")];
+  function handleSelectOption() {
+
+    menuOption.selectOption = this.dataset.option;
+    console.log(menuOption);
+    options.forEach(option => {
+      option.classList.remove("select_option")
+    })
+    this.classList.add("select_option")
+  }
+  options.forEach(option => option.addEventListener("click", handleSelectOption));
+};
+
+
