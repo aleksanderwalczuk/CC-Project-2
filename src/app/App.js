b148c1173f3181885e33d10e4dac80ee9f5e83fd
VieraBoschkova <vieraboschkova@gmail.com>
2021-01-04 19:00:42 +0100

    refactor: change folders structure fix: add rendering from js side

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,8 @@
-import createButtonRed from './components/ButtonRed';
+import ButtonRed from './components/ButtonRed';
 
 const App = ({ options }) => {
-  const optionsContainer = document.querySelector(
-    '.options-container',
-  );
-  const buttonPlay = createButtonRed('play the game');
+  const optionsContainer = document.querySelector('#swquiz-app');
+  const buttonPlay = ButtonRed('play the game');
   optionsContainer.append(buttonPlay);
 };
 export default App;
