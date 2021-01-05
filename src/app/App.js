278e588532649f46ad9e34804ff6f7b543044b1f
SebastianBabinski1 <sebastian.babinski1@gmail.com>
2021-01-05 20:02:10 +0100

    Formating files

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
-import VisualImage from "./components/VisualImage";
-import Logo from "./components/Logo";
+import VisualImage from './components/VisualImage';
+import Logo from './components/Logo';
 import createButtonRed from './components/ButtonRed';
 import createWhiteButtonWithIcon from './components/ButtonWhiteWithIcon';
 
 const App = ({ options }) => {
   Logo();
-  
-  VisualImage("../../static/assets/img/modes/people/1.jpg");
+
+  VisualImage('../../static/assets/img/modes/people/1.jpg');
 
   const quiz = document.querySelector('#swquiz-app');
   const buttonPlay = createButtonRed('play the game');
--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -1,15 +1,15 @@
 function Logo() {
   // Creating elements
-  const elementA = document.createElement("a");
-  const elementDiv = document.createElement("div");
+  const elementA = document.createElement('a');
+  const elementDiv = document.createElement('div');
 
   // Setting atribute and class
-  elementA.setAttribute("href", "./");
-  elementDiv.classList.add("starwars__logo");
+  elementA.setAttribute('href', './');
+  elementDiv.classList.add('starwars__logo');
 
   // Adding to html structure
   elementA.appendChild(elementDiv);
-  document.querySelector("#swquiz-app").appendChild(elementA);
+  document.querySelector('#swquiz-app').appendChild(elementA);
 }
 
 export default Logo;
