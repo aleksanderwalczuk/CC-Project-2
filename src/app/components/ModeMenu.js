9c4fbce73e227e33c6eb3e9c294a0ee9f9924533
paula0403 <paulinalipinskagdy@gmail.com>
2021-01-07 19:35:06 +0100

    Changed component (added after element, deleted attribute), ordered style file.

--- src/app/components/ModeMenu.js
+++ src/app/components/ModeMenu.js
@@ -1,33 +1,24 @@
 function ModeMenu() {
   const menu = document.createElement('nav');
-  menu.classList.add('menu');
-  document.querySelector('#swquiz-app').append(menu);
   const options = ['People', 'Vehicles', 'Starships'];
   options.forEach((option) => {
-    const container = document.createElement('div');
     const btn = document.createElement('button');
-    const div = document.createElement('div');
-    container.classList.add('option');
-    btn.setAttribute('data-option', option);
-    btn.classList.add('option_button');
+    btn.classList.add('menu_button');
     btn.textContent = option;
-    div.classList.add('option_underline');
-    menu.append(container);
-    container.append(btn, div);
+    menu.append(btn);
   });
+  menu.classList.add('menu');
+  document.querySelector('#swquiz-app').append(menu);
   const menuOption = {
     selectOption: '',
   };
-  const menuElements = document.querySelectorAll('.option_button');
+  const menuElements = document.querySelectorAll('.menu_button');
   function handleSelectOption() {
-    menuOption.selectOption = this.dataset.option;
-    menuElements.forEach((optionBtn) => {
-      const btnOption = optionBtn;
-      btnOption.style.color = 'rgba(0, 0, 0, 0.4)';
-      btnOption.nextElementSibling.style.display = 'none';
+    menuOption.selectOption = this.textContent;
+    menuElements.forEach((menuBtn) => {
+      menuBtn.classList.remove('menu_button--select');
     });
-    this.style.color = 'rgba(0,0,0)';
-    this.nextElementSibling.style.display = 'block';
+    this.classList.add('menu_button--select');
   }
   menuElements.forEach((optionBtn) =>
     optionBtn.addEventListener('click', handleSelectOption),
