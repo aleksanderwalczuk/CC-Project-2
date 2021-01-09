d0a33569b3356e75c9f989d52d775f7e777fe364
Dominik <dominik-nowak@hotmail.com>
2021-01-09 19:53:33 +0100

    example of usage elementFactory and render functions

--- src/app/App.js
+++ src/app/App.js
@@ -3,13 +3,14 @@ import Logo from './components/Logo';
 import createButtonRed from './components/ButtonRed';
 import createWhiteButtonWithIcon from './components/ButtonWhiteWithIcon';
 import createGameModeName from './components/GameModeName';
+import elemFactory from './utils/elementFactory';
+import render from './utils/render';
 
 const App = ({ options }) => {
   Logo();
 
   VisualImage('../../static/assets/img/modes/people/1.jpg');
 
-  const quiz = document.querySelector('#swquiz-app');
   const buttonPlay = createButtonRed('play the game');
   const buttonRules = createWhiteButtonWithIcon(
     'Rules',
@@ -17,7 +18,28 @@ const App = ({ options }) => {
     'fa-graduation-cap',
   );
   const gameModeInfo = createGameModeName('Who is this character?');
-  quiz.append(gameModeInfo, buttonRules, buttonPlay);
+
+  //TODO: to remove after accept
+
+  const childOfTestElement = elemFactory(
+    'p',
+    { id: 'childOfTest' },
+    'Example of element',
+  );
+
+  const testElement = elemFactory(
+    'div',
+    { class: 'button button--success', id: 'test' },
+    childOfTestElement,
+  );
+
+  render(
+    'swquiz-app',
+    gameModeInfo,
+    buttonRules,
+    buttonPlay,
+    testElement,
+  );
 };
 
 export default App;
