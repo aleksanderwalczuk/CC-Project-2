2300eb9a64fa76ad960f1677559b8b674ed4f273
SebastianBabinski1 <sebastian.babinski1@gmail.com>
2021-01-10 18:33:31 +0100

    Aplied changes from code review

--- src/app/components/RemainingTime.js
+++ src/app/components/RemainingTime.js
@@ -1,29 +1,39 @@
-function handleTimeChange(element){
-  let timer = 120;
+function handleTimeChange(element) {
+  let timer = process.env.QUIZ_MAX_TIME_SECONDS;
 
   const idInterval = setInterval(() => {
     timer--;
 
-    if(timer === 0){
+    const min = Math.floor(timer / 60);
+    const sec = timer % 60;
+    let minToDisplay = min;
+    let secToDisplay = sec;
+
+    if (timer === 0) {
       clearInterval(idInterval);
-    }
+    };
 
-    let min = Math.floor(timer / 60);
-    let sec = timer % 60;
+    if (min < 10) {
+      minToDisplay = '0' + minToDisplay;
+    };
 
-    element.textContent = `Time Left: ${min}m ${sec}s`;
+    if (sec < 10) {
+      secToDisplay = '0' + secToDisplay;
+    };
+
+    element.textContent = `Time Left: ${minToDisplay}m ${secToDisplay}s`;
   }, 1000);
 };
 
 function RemainingTime() {
-  const root = document.querySelector("#swquiz-app");
+  const root = document.querySelector('#swquiz-app');
 
-  const parentElement = document.createElement("div");
-  parentElement.classList.add("remaining-time");
+  const parentElement = document.createElement('div');
+  parentElement.classList.add('remaining-time');
 
-  const counter = document.createElement("div");
-  counter.textContent = "Time Left : 2 m 00 s";
-  counter.classList.add("remaining-time__counter");
+  const counter = document.createElement('div');
+  counter.textContent = 'Time Left : 02 m 00 s';
+  counter.classList.add('remaining-time__counter');
 
   parentElement.appendChild(counter);
   root.appendChild(parentElement);
