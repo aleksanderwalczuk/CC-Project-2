ad0a5956675d4e01c8ac2c0acdb9687f0852c32b
Aleksandra Pawlak <synowa8@gmail.com>
2021-01-10 20:44:33 +0100

    Create a Mode Rules box
    closes #20  fixex #20

---
+++ src/app/components/ModeRules.js
@@ -0,0 +1,26 @@
+const rulesTexts = {
+  people:
+    'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',
+  vehicles:
+    'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select which vehicle from Star Wars is showed on the left.',
+  starships:
+    'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select which starship from Star Wars is showed on the left.',
+};
+
+const ModeRules = (mode) => {
+  const rulesElement = document.createElement('div');
+  const rulesHeader = document.createElement('header');
+  const rulesIcon = document.createElement('span');
+  const rulesTextElement = document.createElement('div');
+  rulesElement.classList.add('rules');
+  rulesHeader.classList.add('rules-header');
+  rulesIcon.classList.add('rules-icon', 'fa', 'fa-graduation-cap');
+  rulesTextElement.classList.add('rules-text');
+  rulesElement.append(rulesHeader, rulesTextElement);
+  rulesHeader.append(rulesIcon, 'Mode Rules');
+  rulesTextElement.append(rulesTexts[mode] ?? rulesTexts.people);
+
+  return rulesElement;
+};
+
+export default ModeRules;
