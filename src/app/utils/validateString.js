c1dd04a696facd3b9049f9dee41d30d3e190c5c2
VieraBoschkova <vieraboschkova@gmail.com>
2021-01-12 09:17:51 +0100

    refactor: modify string validation function

--- src/app/utils/validateString.js
+++ src/app/utils/validateString.js
@@ -1,13 +1,6 @@
 export default function validateString(text) {
   const isString = typeof text === 'string';
-  if (!isString) {
-    throw TypeError('Invalid data');
-  }
-
   const hasCharacters = text.trim().length > 0;
-  if (!hasCharacters) {
-    throw new Error('Text is Empty');
-  }
 
-  return true;
+  return !isString || !hasCharacters ? 0 : 1;
 }
