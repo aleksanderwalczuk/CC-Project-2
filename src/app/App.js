3cb90256d1ae84ec91338a0e8d87fdaadaa06b7d
Dominik <dominik-nowak@hotmail.com>
2021-01-13 19:39:18 +0100

    clear App.js from elementFactory's playground

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,6 @@ import Logo from './components/Logo';
 import createButtonRed from './components/ButtonRed';
 import createWhiteButtonWithIcon from './components/ButtonWhiteWithIcon';
 import createGameModeName from './components/GameModeName';
-import elemFactory from './utils/elementFactory';
 import render from './utils/render';
 
 const App = ({ options }) => {
@@ -19,47 +18,7 @@ const App = ({ options }) => {
   );
   const gameModeInfo = createGameModeName('Who is this character?');
 
-  //TODO: to remove after accept
-  /* ------------ Examples of usage: -------------*/
-
-  const firstChildOfTestElement = elemFactory(
-    'div',
-    { id: 'firstChild' },
-    'Pierwsze dziecko',
-  );
-
-  const thirdChildOfTestElement = elemFactory(
-    'ul',
-    {},
-    'Trzecie dziecko - lista elementów:',
-    elemFactory('li', { style: 'color: red' }, 'pierwszy punkt'),
-    elemFactory('li', {}),
-    elemFactory('li', {}, 'trzeci punkt'),
-  );
-
-  const testElement = elemFactory(
-    'div',
-    { className: 'button button--success', id: 'test' },
-    firstChildOfTestElement,
-    'To jest drugie dziecko',
-    thirdChildOfTestElement,
-  );
-
-  const childOfFirstChildOfTestElement = elemFactory(
-    'p',
-    {},
-    'Dziecko pierwszego dziecka',
-  );
-
-  render(
-    '#swquiz-app',
-    gameModeInfo,
-    buttonRules,
-    buttonPlay,
-    testElement,
-  );
-
-  render('#firstChild', childOfFirstChildOfTestElement);
+  render('#swquiz-app', gameModeInfo, buttonRules, buttonPlay);
 };
 
 export default App;
