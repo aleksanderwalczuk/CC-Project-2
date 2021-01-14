cf3589f1270bafbc9ae00a57fcacdb0ccfcf18e0
Enessetere <muszalski.sebastian@gmail.com>
2021-01-14 14:31:15 +0100

    refactor: change generation of Logo element to elementFactory

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -1,15 +1,8 @@
+import elementFactory from '../utils/elementFactory';
+
 function Logo() {
-  // Creating elements
-  const elementA = document.createElement('a');
-  const elementDiv = document.createElement('div');
-
-  // Setting atribute and class
-  elementA.setAttribute('href', './');
-  elementDiv.classList.add('starwars__logo');
-
-  // Adding to html structure
-  elementA.appendChild(elementDiv);
-  document.querySelector('#swquiz-app').appendChild(elementA);
+  // Creating element
+  return elementFactory('div', { className: 'starwars__logo' });
 }
 
 export default Logo;
