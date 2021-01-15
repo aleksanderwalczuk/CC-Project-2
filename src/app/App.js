73ce238245f7fedbf35c6ebc35ed0ffaa886f984
VieraBoschkova <vieraboschkova@gmail.com>
2021-01-15 23:32:29 +0100

    fix: move close modal function to separete file

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,5 @@
-import VisualImage from './components/VisualImage';
-import Logo from './components/Logo';
-import createButtonRed from './components/ButtonRed';
-import createWhiteButtonWithIcon from './components/ButtonWhiteWithIcon';
-import createGameModeName from './components/GameModeName';
-
 import ModalWindow from './components/ModalWindow';
+import handleModalClose from './logic/handleModalClose'
 // MOCK DATA TO BE REMOVED
 import {
   game,
@@ -16,21 +11,12 @@ import elemFactory from './utils/elementFactory';
 import render from './utils/render';
 
 const App = ({ options }) => {
-  // FUNCTION TO BE PASSED TO MODAL - EXAMPLE - TO BE REMOVED FROM APP PROBABLY
-  function closeModal(event) {
-    event.preventDefault();
-    const playersName = document.querySelector('.player-form__input')
-      .value;
-    // quiz.removeChild(document.querySelector('.modal'));
-    console.log(`close the modal ${playersName}`);
-    return playersName;
-  }
   // CREATE MODAL WINDOW
   const modalWindow = ModalWindow(
     game,
     humanPlayerGame,
     computerPlayerGame,
-    closeModal,
+    handleModalClose,
   );
   // SHOULD BE APPENDED WHEN THE TIME ENDS - TO BE REMOVED
   render('#swquiz-app', modalWindow);
