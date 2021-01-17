dbbc2690caed16146b3788d25b4ffd92a578af41
Enessetere <muszalski.sebastian@gmail.com>
2021-01-17 18:10:32 +0100

    fix: fix call function in test

--- test/quiz.spec.js
+++ test/quiz.spec.js
@@ -1,9 +1,9 @@
-import { getQuestion } from '../src/app/logic/quiz';
+import generateQuestion from '../src/app/logic/quiz';
 
 describe('Game process tests', () => {
   it('Should return empty object, if question scope is unset', () => {
     const expectedValue = { err: 'modeOrDataError' };
-    const question = getQuestion();
+    const question = generateQuestion();
     expect(question).toEqual(expectedValue);
   });
 });
