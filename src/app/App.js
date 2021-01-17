7326539642d9fc2f85a6497cf4e4176baf6d5f88
SebastianBabinski1 <sebastian.babinski1@gmail.com>
2021-01-17 16:27:47 +0100

    Added layout during game

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,10 @@
 import { LoadPage } from './components/MainMenu';
+import { startGame, displayAnswer } from './components/GameOn'
 
 const App = ({ options }) => {
   LoadPage();
+  startGame(true, 'people');
+  displayAnswer(['raz', 'dwa', 'trzy', 'cztery'], 'trzy');
 };
 
 export default App;
---
+++ src/app/components/GameOn.js
@@ -0,0 +1,32 @@
+import Answers from './Answers'
+import RemainingTime from './RemainingTime'
+import GameModeName from './GameModeName';
+import { PEOPLE } from '../constants';
+import elementFactory from '../utils/elementFactory';
+
+const startGame = (gameStarted, choosenMode) => {
+  if(gameStarted === true) {
+    const section = document.querySelector('.section');
+    const sectionWrapper = document.querySelector('.section__wrapper');
+    const placeholder = elementFactory(
+      'div',
+      { className: 'placeholder' }
+    )
+
+    sectionWrapper.innerHTML = '';
+    sectionWrapper.appendChild(GameModeName(choosenMode));
+    sectionWrapper.appendChild(placeholder);
+
+    section.appendChild(RemainingTime());  
+  }
+}
+
+const displayAnswer = (answers, correctAnswer, onAnsweredQuestion, callback) => {
+  const sectionWrapper = document.querySelector('.section__wrapper');
+  const placeholder = document.querySelector('.placeholder');
+
+  sectionWrapper.removeChild(placeholder);
+  sectionWrapper.appendChild(Answers(answers, correctAnswer, onAnsweredQuestion, callback));
+}
+
+export { startGame, displayAnswer };
