d439241b207fc044c2eecc2130f11613d52d2ef7
Dominik <dominik-nowak@hotmail.com>
2021-01-18 04:45:49 +0100

    refactor GameOn

--- src/app/components/GameOn.js
+++ src/app/components/GameOn.js
@@ -1,40 +1,28 @@
 import Answers from './Answers';
 import RemainingTime from './RemainingTime';
 import GameModeName from './GameModeName';
-import { PEOPLE } from '../constants';
 import elementFactory from '../utils/elementFactory';
 
-const startGame = (gameStarted, choosenMode) => {
-  if (gameStarted === true) {
-    const section = document.querySelector('.section');
-    const sectionWrapper = document.querySelector(
-      '.section__wrapper',
-    );
-    const placeholder = elementFactory('div', {
-      className: 'placeholder',
-    });
+const handleGame = (chosenMode) => {
+  const section = document.querySelector('.section');
+  const sectionWrapper = document.querySelector('.section__wrapper');
+  const answersWrapper = elementFactory('div', {
+    className: 'answers-wrapper',
+  });
 
-    sectionWrapper.innerHTML = '';
-    sectionWrapper.appendChild(GameModeName(choosenMode));
-    sectionWrapper.appendChild(placeholder);
+  sectionWrapper.innerHTML = '';
+  sectionWrapper.appendChild(GameModeName(chosenMode));
+  sectionWrapper.appendChild(answersWrapper);
 
-    section.appendChild(RemainingTime());
-  }
+  section.appendChild(RemainingTime());
 };
 
-const displayAnswer = (
-  answers,
-  correctAnswer,
-  onAnsweredQuestion,
-  callback,
-) => {
-  const sectionWrapper = document.querySelector('.section__wrapper');
-  const placeholder = document.querySelector('.placeholder');
-
-  sectionWrapper.removeChild(placeholder);
-  sectionWrapper.appendChild(
-    Answers(answers, correctAnswer, onAnsweredQuestion, callback),
+const displayAnswers = (answers, correctAnswer, humanPlayer) => {
+  const answersWrapper = document.querySelector('.answers-wrapper');
+  answersWrapper.innerHTML = '';
+  answersWrapper.appendChild(
+    Answers(answers, correctAnswer, humanPlayer),
   );
 };
 
-export { startGame, displayAnswer };
+export { handleGame, displayAnswers };
